28ba21fb72f22b25ac338afcf8bb8798ad5b7936
Leomir <leomir2002@gmail.com>
2023-04-11 04:04:16 +0300

    Just working with phone

---
+++ Fourth/Simple-online-chat/resources/views/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.layout')
+
+@section('title','Simple online chat')
+
+
+@section('content')
+
+    <div class="main">
+        <div class="first">
+        <h1>Hi!</h1><br>
+            <h2>Are you new here?</h2><br>
+            <h3>press here to register or here to log in</h3>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+@endsection
---
+++ Fourth/Simple-online-chat/resources/views/layout/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<head>
+    <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>@yield('title')</title>
+</head>
+<body>
+
+@yield('content')
+
+</body>
+
+@yield('scripts')
+</HTML>
