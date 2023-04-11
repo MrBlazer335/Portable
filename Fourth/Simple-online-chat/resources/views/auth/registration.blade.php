697c34611539ed12d8169439f7045c783eeb788e
Leomir <leomir2002@gmail.com>
2023-04-11 08:36:06 +0300

    new buttons

--- Fourth/Simple-online-chat/resources/views/auth/registration.blade.php
+++ Fourth/Simple-online-chat/resources/views/auth/registration.blade.php
@@ -4,7 +4,30 @@
 
 @section('content')
 
-    <div class="main">damn</div>
+    <div class="main">
+        <div class="form">
+            <h2>Simple-chat</h2>
+            <form method="post">
+                <ul>
+
+                    <li><label>Insert name</label>
+                        <input type="text" name="name"></li>
+                    <li><label>Insert Email: </label><input type="email" name="email"></li>
+                <li><label>Insert password: </label><input type="password" name="password"></li>
+                <li><label>Confirm password: </label><input type="password" name="conf_password"></li>
+                    <li><button type="submit">Sign-up Now</button></li>
+                </ul>
+            </form>
+        </div>
+
+
+
+
+
+
+
+
+    </div>
 
 
 
--- Fourth/Simple-online-chat/resources/views/index.blade.php
+++ Fourth/Simple-online-chat/resources/views/index.blade.php
@@ -9,9 +9,9 @@
         <div class="first">
         <h1>Hi!</h1><br>
             <h2>Are you new here?</h2><br>
-            <h2><button class="btn-reg"><a href="{{route('registration')}}">Registration</a> </button> </h2>
+            <a href="{{route('registration')}}" class="btn-reg">Registration</a>
             <h2>or</h2>
-            <h2><button class="btn-log"><a href="{{route('login')}}">Log in</a></button> </h2>
+            <a href="{{route('login')}}" class="btn-log">Log in</a>
         </div>
     </div>
 
