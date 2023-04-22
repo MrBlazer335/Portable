1bdd271da4cec24326f1a14472660c22a4aa4885
Leomir <leomir2002@gmail.com>
2023-04-22 19:38:40 +0300

    Design

--- Fourth/Simple-online-chat/resources/views/index.blade.php
+++ Fourth/Simple-online-chat/resources/views/index.blade.php
@@ -3,16 +3,12 @@
 @section('title','HomePage')
 
 @section('content')
+    <img class="left-top" src="{{asset('img/Triangle with Circle.png')}}" alt="Design">
+    <img class="left-bottom" src="{{asset('img/Double Square.png')}}" alt="Design">
+    <img class="right-top" src="{{asset('img/Double Square.png')}}" alt="Design">
+    <img class="right-bottom" src="{{asset('img/Circle with Triangle.png')}}" alt="Damn">
+    <div class="container">
+        <span>Welcome!</span>
+    </div>
 
-        <div class="col-4 position-absolute top-0 start-0 p-0 m-0 d-none d-md-block">
-            <img src="{{ asset('img/Triangle with Circle.png') }}" alt="Square">
-        </div>
-    <div class="d-md-none col-2 d-flex flex-column h-20" style="width: 30%; height: 5%;"><img src="{{ asset('img/Triangle with Circle.png') }}" alt="Square"></div>
-        <div class="col-4 position-absolute bottom-0 start-0 p-0 m-0 d-none d-md-block">
-            <img src="{{ asset('img/Double Square.png') }}" alt="Square">
-        </div>
-        <div class="d-md-none col-2 d-flex flex-column h-30 fixed-bottom" style="width: 30%; height: 14%;"><img src="{{ asset('img/Double Square.png') }}" alt="Square"></div>
-        <div class="col-1.5 position-absolute top-0 end-0 p-0 m-0 d-none d-md-block" style="right: 0; top: 0;">
-            <img src="{{asset('img/Double Square.png')}}" alt="Square">
-        </div>
 @endsection
