cfa4cb176c4e6d87f6bc958c5d63d362c0404204
Leomir <leomir2002@gmail.com>
2023-04-22 20:22:09 +0300

    Optimization

--- Fourth/Simple-online-chat/resources/views/index.blade.php
+++ Fourth/Simple-online-chat/resources/views/index.blade.php
@@ -7,8 +7,16 @@
     <img class="left-bottom" src="{{asset('img/Double Square.png')}}" alt="Design">
     <img class="right-top" src="{{asset('img/Double Square.png')}}" alt="Design">
     <img class="right-bottom" src="{{asset('img/Circle with Triangle.png')}}" alt="Damn">
-    <div class="container">
         <span>Welcome!</span>
-    </div>
+            <form>
+                <label for="input-field" class="mb-3">Login</label>
+                <input type="text" id="input-field" name="Login" class="mb-4" placeholder="✉ | Exemple@gmail.com">
+                <label for="input-field" class="mb-3">Password</label>
+                <input type="password" id="input-field" name="Password" placeholder="🔒 | ********">
+                <a href="#" class="btn-link text-primary text-decoration-none fw-bold mt-2">Forget password?</a>
+                <button type="submit" class="btn btn-lg bg-primary text-white mt-3">Log in</button>
+            </form>
+        </div>
+
 
 @endsection
