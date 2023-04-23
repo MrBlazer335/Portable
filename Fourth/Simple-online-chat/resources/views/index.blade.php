608fa85775ba9b8970a1e32584eec4c98c12ee63
Leomir <leomir2002@gmail.com>
2023-04-23 03:03:10 +0300

    Done?

--- Fourth/Simple-online-chat/resources/views/index.blade.php
+++ Fourth/Simple-online-chat/resources/views/index.blade.php
@@ -9,14 +9,16 @@
     <img class="right-bottom" src="{{asset('img/Circle with Triangle.png')}}" alt="Damn">
         <span>Welcome!</span>
             <form>
-                <label for="input-field" class="mb-3">Login</label>
-                <input type="text" id="input-field" name="Login" class="mb-4" placeholder="✉️ | Exemple@gmail.com">
-                <label for="input-field" class="mb-3">Password</label>
-                <input type="password" id="input-field" name="Password" placeholder="🔒 | ********">
+                <label for="input-field1" class="mb-3">Login</label>
+                <input type="text" id="input-field1" name="Login" class="mb-4" placeholder="✉️ | Exemple@gmail.com">
+                <label for="input-field2" class="mb-3">Password</label>
+                <input type="password" id="input-field2" name="Password" placeholder="🔒 | ********">
                 <a href="#" class="btn-link text-primary text-decoration-none fw-bold mt-2">Forget password?</a>
                 <button type="submit" class="btn btn-lg bg-primary text-white mt-3">Log in</button>
+                <hr class="mt-4">
+                    <img src="{{asset('img/icon-google.svg')}}" class="icon-g me-2" alt="Google">
+                <p class="mt-5">Dont have account? <a href="#" class="btn btn-link text-decoration-none fw-bold text-primary">Sign up</a></p>
             </form>
-        </div>
 
 
 @endsection
